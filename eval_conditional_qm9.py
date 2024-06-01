7e5da94bedd31bc4957aa826d6b5106a8fb1668c
YuFei Xu <2412453584@qq.com>
2024-06-01 13:48:12 +0800

    save before modify condition gen test

--- eval_conditional_qm9.py
+++ eval_conditional_qm9.py
@@ -11,7 +11,7 @@ from qm9.property_prediction.main_qm9_prop import test
 from qm9.property_prediction import main_qm9_prop
 from qm9.sampling import sample_chain, sample, sample_sweep_conditional
 import qm9.visualizer as vis
-from mypy.utils.molecule_transform import srd_to_xyz
+from mypy.utils.molecule_transform import srd_to_smiles
 
 
 def get_classifier(dir_path='', device='cpu'):
@@ -100,8 +100,8 @@ class DiffusionDataloader:
 
         atom_types = one_hot.argmax(dim=2)
         # TODO convert srd positions to real positions
-        flag, x = srd_to_xyz(x, node_mask, atom_types)
-        print(flag, x)
+        smiles = srd_to_smiles(x, node_mask, atom_types)
+        print(smiles)
 
         node_mask = node_mask.squeeze(2)
         context = context.squeeze(1)
@@ -120,11 +120,10 @@ class DiffusionDataloader:
         else:
             context = context * self.prop_dist.normalizer[prop_key]['mad'] + self.prop_dist.normalizer[prop_key]['mean']
         data = {
-            'positions': x.detach(),
+            'smiles': smiles,
             'atom_mask': node_mask.detach(),
             'edge_mask': edge_mask.detach(),
             'one_hot': one_hot.detach(),
-            'flag': flag.detach(),
             prop_key: context.detach()
         }
         return data
