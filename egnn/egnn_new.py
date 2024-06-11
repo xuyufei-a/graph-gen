23df101d2f6f3877006f267014503de17fe02d94
yufei <2412453584@qq.com>
2024-06-11 19:12:24 +0800

    no batch norm

--- egnn/egnn_new.py
+++ egnn/egnn_new.py
@@ -148,7 +148,7 @@ class EquivariantBlock(nn.Module):
         distances, coord_diff = coord2diff(x, edge_index, self.norm_constant)
         if self.sin_embedding is not None:
             distances = self.sin_embedding(distances)
-        distances = self.batch_norm(distances)
+#         distances = self.batch_norm(distances)
         edge_attr = torch.cat([distances, edge_attr], dim=1)
         for i in range(0, self.n_layers):
             h, _ = self._modules["gcl_%d" % i](h, edge_index, edge_attr=edge_attr, node_mask=node_mask, edge_mask=edge_mask)
@@ -174,7 +174,7 @@ class EGNN(nn.Module):
         self.norm_diff = norm_diff
         self.normalization_factor = normalization_factor
         self.aggregation_method = aggregation_method
-        self.batch_norm = nn.BatchNorm1d(num_features=1)
+#         self.batch_norm = nn.BatchNorm1d(num_features=1)
 
         if sin_embedding:
             self.sin_embedding = SinusoidsEmbeddingNew()
